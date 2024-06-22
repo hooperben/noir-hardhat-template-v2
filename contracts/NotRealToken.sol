62df58b89686b993140a914af5b9e4bd174914bc
Ben Hooper <ben@labrys.io>
2024-06-22 17:05:06 +0200

    feat: clarified that this is not a real token

--- contracts/NotRealToken.sol
+++ contracts/NotRealToken.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract NOTRealToken is ERC20 {
+contract NotRealToken is ERC20 {
     constructor() ERC20("NOTRealToken", "NOTREAL") {
         _mint(msg.sender, 1000000 * 10 ** decimals());
     }
